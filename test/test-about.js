702b24271495b7d0f6efb72854dda46edc0f00ca
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-12-24 13:59:51 -0800

    📝 planning for TDD approach; adjusted README; other minor edits

---
+++ test/test-about.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('About', () => {
+    // TEST ROOT
+    it('should display quiz on / GET', (done) => {
+        chai.request(server)
+            .get('/')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
+            });
+    });
+});
