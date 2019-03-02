cc4e2984b07789be0f4a2dad63f640ed583073be
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-03-02 15:06:01 -0800

    📋 refactored signup route to use async await

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,71 +29,25 @@ module.exports = (app) => {
     });
 
     // SIGN-UP POST
-    app.post('/sign-up', (req, res) => {
-        const username = req.body.username;
-        const email = req.body.email;
-        console.log('username', username, 'email', email);
-        // CHECK FOR DUPLICATE USER
-        User.findOne({ username })
-            .then((oldUser) => {
-                if (!oldUser || oldUser == null) {
-                    User.findOne({ email })
-                        .then((emailUser) => {
-                            if (!emailUser || emailUser == null) {
-                                // CREATE User
-                                const user = new User(req.body);
-                                user
-                                    .save()
-                                    .then(() => {
-                                        const token = jwt.sign({
-                                            _id: user._id,
-                                        }, process.env.SECRET, {
-                                            expiresIn: '60 days',
-                                        });
-                                        res.cookie('nameToken', token, {
-                                            maxAge: 900000,
-                                            httpOnly: true,
-                                        });
-                                        const sendEmail = {
-                                            email: req.body.email,
-                                            username: req.body.username,
-                                        };
-                                        nodemailerMailgun.sendMail({
-                                            from: 'no-reply@name-ly.com',
-                                            to: sendEmail.email,
-                                            subject: `Welcome, ${sendEmail.username}`,
-                                            template: {
-                                                name: path.join(__dirname, '..', '/views/email.handlebars'),
-                                                engine: 'handlebars',
-                                                context: user,
-                                            },
-                                        }).then((info) => {
-                                            console.log(`Response: ${info}`);
-                                            res.redirect(`/users/${user._id}`);
-                                        })
-                                            .catch((err) => {
-                                                console.log(`Error: ${err}`);
-                                                res.redirect(`/users/${user._id}`);
-                                            });
-                                    })
-                                    .catch((err) => {
-                                        console.log(err.message);
-                                        return res.status(400).send({ err });
-                                    });
-                            } else {
-                                res.send('That email address is already in use! Please go back and login.');
-                            }
-                        })
-                        .catch((err) => {
-                            console.log(err.message);
-                        });
-                } else {
-                    res.send('That username is already in use! Please go back and login.');
-                }
-            })
-            .catch((err) => {
-                console.log(err.message);
+    app.post('/sign-up', async (req, res) => {
+        let user;
+        try {
+            user = await new User(req.body);
+            await user.save();
+        } catch (err) {
+            console.log(err);
+        }
+        let token;
+        try {
+            token = await jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: '60 days' });
+            res.cookie('nameToken', token, {
+                maxAge: 900000,
+                httpOnly: true,
             });
+            res.redirect(`/users/${user._id}`);
+        } catch (err) {
+            console.log(err);
+        }
     });
 
     // LOGIN
